50cb273f702410645495265e8d948b92e5b1f1b5
Peter James Flanagan <pjflanagan1@gmail.com>
2019-04-01 23:43:21 -0400

    functioning version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 
-function main() {
+function make_body_cell(conf) {
+	return `
+		<td class="tight-cell Table2__td">${conf}</td>
+	`;
+}
+
+function add_body() {
 	$('.Table2__table__wrapper').find('tr[data-idx]').each(function () {
-		console.log($(this).text());
-		// $(this).find('td').eq(n).after('<td>new cell added</td>');
+		const name = $(this).find('td a[data-clubhouse-uid] img').attr('alt');
+		const team = _.find(TEAMS, { 'name': name });
+		let conf = '';
+		if (!!team) {
+			conf = team.conference_abbr
+		}
+		$(this).append(make_body_cell(conf));
 	});
 }
 
+function make_header_cell() {
+	return `
+		<th title class="Table2__th">
+			CONF
+		</th>
+	`;
+}
+
+function add_header() {
+	$('thead.Table2__thead tr').append(make_header_cell());
+}
+
 window.onload = function () {
-	main();
+	add_header();
+	add_body();
 };
